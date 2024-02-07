b4dabdf75caba5394f20bd76d0cbfbd0c6c0ea3f
Alex <alex.brock@lego.com>
2024-02-07 15:08:14 -0800

    next install and general setup

---
+++ app/(studio)/my-studio/upload/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>My studio upload</h1>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Card from '@common/card';
+import Icon from '@common/icon';
+
+export default function Page() {
+  return ( 
+    <>
+      <h1>Hello, Next.js!</h1>
+      <Card/>
+      <Icon/>
+    </>
+  )
+}
